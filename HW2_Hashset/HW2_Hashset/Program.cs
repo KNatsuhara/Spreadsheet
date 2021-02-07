33bc8506c878fdb48bab7d7e36362d7b9a307b51
KNatsuhara <kojiman@live.com>
2021-02-07 00:04:21 -0800

    Set up stylecop and .json file as well as added the header to the program.cs file

---
+++ HW2_Hashset/HW2_Hashset/Program.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="Program.cs" company="Koji Natsuhara">
+// Copyright (c) Koji Natsuhara. All rights reserved.
+// </copyright>
+
+namespace HW2_Hashset
+{
+    /// <summary>
+    /// This program will run the main method.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [System.STAThread]
+        public static void Main()
+        {
+            System.Windows.Forms.Application.EnableVisualStyles();
+            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+            System.Windows.Forms.Application.Run(new Form1());
+        }
+    }
+}
