8045f8e089079cfe122d5e1c273c857928b7f599
KNatsuhara <kojiman@live.com>
2021-02-11 17:33:52 -0800

    Added NUnitTest1 and documented form1.cs to meet stylecop requirements

--- HW2_Hashset/HW2_Hashset/Form1.cs
+++ HW2_Hashset/HW2_Hashset/Form1.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// <copyright file="Form1.cs" company="Koji Natsuhara">
+// Copyright (c) Koji Natsuhara. All rights reserved.
+// </copyright>
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +14,18 @@ using System.Windows.Forms;
 
 namespace HW2_Hashset
 {
+    /// <summary>
+    /// This class will create a form object.
+    /// </summary>
     public partial class Form1 : Form
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Form1"/> class.
+        /// This function will initialize a form.
+        /// </summary>
         public Form1()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
     }
 }
