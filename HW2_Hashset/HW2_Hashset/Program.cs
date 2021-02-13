95658515491f9cbbdf349579606d21f5c18bb295
KNatsuhara <kojiman@live.com>
2021-02-12 16:22:28 -0800

    Added and Documented PrintToForm method and outputted the correct strings to the form.

--- HW2_Hashset/HW2_Hashset/Program.cs
+++ HW2_Hashset/HW2_Hashset/Program.cs
@@ -16,29 +16,41 @@ namespace HW2_Hashset
         [System.STAThread]
         public static void Main()
         {
-            int temp = 0;
+            int hashResult = 0, storageResult = 0, sortResult = 0, temp = 0;
             System.Windows.Forms.Application.EnableVisualStyles();
             System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
             Form1 textForm = new Form1(); // Creating Form1 object called textForm.
             var rand = new System.Random(); // Instantiate random number generator.
             System.Collections.Generic.List<int> randomList = new System.Collections.Generic.List<int>(); // Creates an empty generic list.
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 10000; i++)
             {
-                temp = rand.Next(0, 10); // Creating random integers from 0 to 20,000.
+                temp = rand.Next(0, 20001); // Creating random integers from 0 to 20,000.
                 randomList.Add(temp); // Adding random integers to randomList.
-                textForm.PrintResults(temp.ToString());
             }
 
-            temp = FindDistinctUsingHashSet(randomList);
-            string hashSetResult = "HashSet Method: " + temp + " unique numbers.";
-            temp = FindDistinctLimitStorage(randomList);
-            string limitStorageResult = "O(1) storage complexity Method: " + temp + " unique numbers.";
-            temp = FindDistinctUsingSort(randomList);
-            string sortListResult = "Sorting Method: " + temp + " unique numbers.";
+            hashResult = FindDistinctUsingHashSet(randomList); // Sets hashResult using Hash Method.
+            storageResult = FindDistinctLimitStorage(randomList); // Sets storageResult using Storage Method.
+            sortResult = FindDistinctUsingSort(randomList); // Sets sortResult using Sorted Method.
+            PrintToForm(hashResult, storageResult, sortResult, textForm); // Outputs all method results to form application.
+
+            System.Windows.Forms.Application.Run(textForm); // Runs updated textForm.
+        }
+
+        /// <summary>
+        /// Will print out 3 lines of text to the form application that on how many distinct numbers there are in the list.
+        /// </summary>
+        /// <param name="hashResult">Distinct numbers in throught the hash method.</param>
+        /// <param name="storageResult">Distinct numbers in throught the storage method.</param>
+        /// <param name="sortResult">Distinct numbers in throught the sorted method.</param>
+        /// <param name="textForm">Name of the form1 object.</param>
+        public static void PrintToForm(int hashResult, int storageResult, int sortResult, Form1 textForm)
+        {
+            string hashSetResult = "1. HashSet method: " + hashResult + " unique numbers.";
+            string limitStorageResult = "2. O(1) storage method: " + storageResult + " unique numbers.";
+            string sortListResult = "3. Sorted method: " + sortResult + " unique numbers.";
             textForm.PrintResults(hashSetResult); // Prints finished result to textBoxResult in textForm.
             textForm.PrintResults(limitStorageResult); // Prints finished result to textBoxResult in textForm.
             textForm.PrintResults(sortListResult); // Prints finished result to textBoxResult in textForm.
-            System.Windows.Forms.Application.Run(textForm); // Runs updated textForm.
         }
     }
 }
