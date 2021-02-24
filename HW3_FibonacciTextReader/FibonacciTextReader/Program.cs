2252026d7a256786c90450f1237fefec6559e859
KNatsuhara <kojiman@live.com>
2021-02-23 23:43:48 -0800

    Created Solution Project and added StyleCop and NUnitTests

---
+++ HW3_FibonacciTextReader/FibonacciTextReader/Program.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="Program.cs" company="Koji Natsuhara (ID: 11666900)">
+// Copyright (c) Koji Natsuhara (ID: 11666900). All rights reserved.
+// </copyright>
+
+namespace FibonacciTextReader
+{
+    /// <summary>
+    /// This program will run application.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [System.STAThread]
+        public static void Main()
+        {
+            System.Windows.Forms.Application.EnableVisualStyles();
+            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+            System.Windows.Forms.Application.Run(new Form1());
+        }
+    }
+}
---
+++ HW3_FibonacciTextReader/NUnitTests/FibonacciTests.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="FibonacciTests.cs" company="Koji Natsuhara (ID: 11666900)">
+// Copyright (c) Koji Natsuhara (ID: 11666900). All rights reserved.
+// </copyright>
+
+using NUnit.Framework;
+
+namespace NUnitTests
+{
+    /// <summary>
+    /// This class will test all the Fibonacci Methods.
+    /// </summary>
+    [TestFixture]
+    public class FibonacciTests
+    {
+        /// <summary>
+        /// Basic Test method sample.
+        /// </summary>
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            var answer = 42;
+            Assert.That(answer, Is.EqualTo(42), "Some useful error message");
+        }
+    }
+}
