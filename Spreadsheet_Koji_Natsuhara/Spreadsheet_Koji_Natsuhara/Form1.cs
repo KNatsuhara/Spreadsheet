3a64b3fd25407a690a4d5bd60505b71d8ac4df7c
KNatsuhara <kojiman@live.com>
2021-02-27 18:48:30 -0800

    Added StyleCop and NUnitTests to the solution project and documented the files.

---
+++ Spreadsheet_Koji_Natsuhara/Spreadsheet_Koji_Natsuhara/Form1.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="Form1.cs" company="Koji Natsuhara (ID: 11666900)">
+// Copyright (c) Koji Natsuhara (ID: 11666900). All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spreadsheet_Koji_Natsuhara
+{
+    /// <summary>
+    /// This class will intitalize the winform application.
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Form1"/> class.
+        /// </summary>
+        public Form1()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
