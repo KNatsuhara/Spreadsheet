18af3dd9297ade11254379a4d06dc734571eb15c
KNatsuhara <kojiman@live.com>
2021-03-02 01:21:20 -0800

    Added and documented the SetupGridColumns and SetupGridRows methods to set the data view grid to a fixed size programmatically.

--- Spreadsheet_Koji_Natsuhara/Spreadsheet_Koji_Natsuhara/Form1.cs
+++ Spreadsheet_Koji_Natsuhara/Spreadsheet_Koji_Natsuhara/Form1.cs
@@ -25,6 +25,37 @@ namespace Spreadsheet_Koji_Natsuhara
         public Form1()
         {
             this.InitializeComponent();
+            this.SetupGridColumns();
+            this.SetupGridRows();
+        }
+
+        /// <summary>
+        /// This function will setup the columns of the dataGridView1 object from A to Z.
+        /// </summary>
+        private void SetupGridColumns()
+        {
+            int alpha = 65; // ASCII VALUE 65 == A
+
+            for (int i = 0; i < 26; i++)
+            {
+                char c = Convert.ToChar(alpha); // CONVERT INT TO CHAR
+                this.dataGridView1.Columns.Add(c.ToString(), c.ToString()); // Setting header text and name to c.
+                alpha++; // INCREMENT alpha
+            }
+        }
+
+        /// <summary>
+        /// This function will setup the rows of the dataGridView1 object from 1 to 50.
+        /// </summary>
+        private void SetupGridRows()
+        {
+            int j = 1;
+            for (int i = 0; i < 50; i++)
+            {
+                this.dataGridView1.Rows.Add();
+                this.dataGridView1.Rows[i].HeaderCell.Value = j.ToString(); // Names the header cell values.
+                j++;
+            }
         }
     }
 }
