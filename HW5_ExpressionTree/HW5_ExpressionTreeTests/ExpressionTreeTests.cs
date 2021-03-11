adb549c8be5d6c078ec11e273893989f295c0fe6
KNatsuhara <kojiman@live.com>
2021-03-11 01:39:22 -0800

    Documented and tested the VariableNode class.

--- HW5_ExpressionTree/HW5_ExpressionTreeTests/ExpressionTreeTests.cs
+++ HW5_ExpressionTree/HW5_ExpressionTreeTests/ExpressionTreeTests.cs
@@ -4,6 +4,7 @@
 
 namespace HW5_ExpressionTreeTests
 {
+    using System.Collections.Generic;
     using CptS321;
     using NUnit.Framework;
 
@@ -27,6 +28,31 @@ namespace HW5_ExpressionTreeTests
             Assert.That(1.0000001, Is.EqualTo(constant2.Evaluate()), "Failed on constant2");
             Assert.That(double.MaxValue, Is.EqualTo(constant3.Evaluate()), "Failed on constant3");
             Assert.That(double.MinValue, Is.EqualTo(constant4.Evaluate()), "Failed on constant4");
+        }
+
+        /// <summary>
+        /// Tests the constructor and evaluate method of the VariableNode class.
+        /// </summary>
+        [Test]
+        public void TestVariableNode()
+        {
+            Dictionary<string, double> reference = new Dictionary<string, double>();
+            reference.Add("test1", 3.4);
+            reference.Add("test2", -1.2);
+            reference.Add("max", double.MaxValue);
+            reference.Add("min", double.MinValue);
+
+            VariableNode variable1 = new VariableNode("test1", ref reference);
+            VariableNode variable2 = new VariableNode("test2", ref reference);
+            VariableNode variable3 = new VariableNode("notFound", ref reference);
+            VariableNode variable4 = new VariableNode("max", ref reference);
+            VariableNode variable5 = new VariableNode("min", ref reference);
+
+            Assert.That(3.4, Is.EqualTo(variable1.Evaluate()), "Failed on variable1");
+            Assert.That(-1.2, Is.EqualTo(variable2.Evaluate()), "Failed on variable2");
+            Assert.That(0, Is.EqualTo(variable3.Evaluate()), "Failed on variable3");
+            Assert.That(double.MaxValue, Is.EqualTo(variable4.Evaluate()), "Failed on variable4");
+            Assert.That(double.MinValue, Is.EqualTo(variable5.Evaluate()), "Failed on variable5");
         }
     }
 }
