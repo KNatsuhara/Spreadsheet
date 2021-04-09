26d2114ac43ea551c536953a1447ed0729524e55
KNatsuhara <kojiman@live.com>
2021-04-08 21:08:52 -0700

    Added HW8_Spreadsheet Undo/Redo System Implementation.

---
+++ HW8_Spreadsheet Formula Evalution/SpreadsheetEngine/Spreadsheet.cs
@@ -0,0 +1,242 @@
+﻿// <copyright file="Spreadsheet.cs" company="Koji Natsuhara (ID: 11666900)">
+// Copyright (c) Koji Natsuhara (ID: 11666900). All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CptS321
+{
+    /// <summary>
+    /// This is the Spreadsheet class that will set up the cell.
+    /// </summary>
+    public class Spreadsheet
+    {
+        /// <summary>
+        /// This is the 2D array that will contain all the cells in the spreadsheet.
+        /// </summary>
+        private SpreadsheetCellValue[,] cellGrid;
+
+        /// <summary>
+        /// Contains the number of rows intialized when creating the spreadsheet grid.
+        /// </summary>
+        private int rowCount;
+
+        /// <summary>
+        /// Contains the number of columns intialized when creating the spreadsheet grid.
+        /// </summary>
+        private int columnCount;
+
+        /// <summary>
+        /// Expression tree which evaluates the cells text.
+        /// </summary>
+        private ExpressionTree expressionTree = new ExpressionTree(string.Empty);
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Spreadsheet"/> class.
+        /// </summary>
+        /// <param name="numRows">Set number of rows in spreadsheet.</param>
+        /// <param name="numColumns">Set number of columns in spreadsheet.</param>
+        public Spreadsheet(int numRows, int numColumns)
+        {
+            this.rowCount = numRows;
+            this.columnCount = numColumns;
+
+            this.cellGrid = new SpreadsheetCellValue[numRows, numColumns];
+
+            for (int i = 0; i < numRows; i++)
+            {
+                for (int j = 0; j < numColumns; j++)
+                {
+                    SpreadsheetCellValue newCell = SpreadsheetCellValue.CreateCell(i, j);
+                    this.cellGrid[i, j] = newCell;
+                    this.cellGrid[i, j].PropertyChanged += this.SpreadsheetCellValue_PropertyChanged;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Declares the property changed event.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChangedValue = (sender, e) => { };
+
+        /// <summary>
+        /// This method will evaluate the text of the string and decide if it is a value or not.
+        /// </summary>
+        /// <param name="text">Text.</param>
+        /// <returns>True if the text starts with "=" or false otherwise.</returns>
+        public static bool CheckText(string text)
+        {
+            if (text == string.Empty)
+            {
+                return false; // Returns false if the string is empty.
+            }
+
+            char eq = text[0]; // Will check the first character in the string.
+
+            if (eq == '=' && text.Length > 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// This function will evaluate the text in the cell and return the value of the cell. This is assuming
+        /// that the text starts with and "=".
+        /// </summary>
+        /// <param name="cellText">Cell text.</param>
+        /// <returns>String of the evaluated text.</returns>
+        public string EvaluateText(string cellText)
+        {
+            string text = cellText.Substring(1, cellText.Length - 1);
+            this.expressionTree.CreateExpressionTree(text);
+            return this.expressionTree.Evaluate().ToString();
+        }
+
+        /// <summary>
+        /// Gets the cell at a specific location or returns null if there is no such cell.
+        /// </summary>
+        /// <param name="row">Row Index.</param>
+        /// <param name="col">Column Index.</param>
+        /// <returns>Abstract SpreadsheetCell class.</returns>
+        public SpreadsheetCell GetCell(int row, int col)
+        {
+            if (this.cellGrid[row, col] == null)
+            {
+                return null;
+            }
+            else
+            {
+                return this.cellGrid[row, col];
+            }
+        }
+
+        /// <summary>
+        /// Sets the cell text in the cell grid.
+        /// </summary>
+        /// <param name="row">Row.</param>
+        /// <param name="col">Column.</param>
+        /// <param name="text">Text.</param>
+        public void SetCellText(int row, int col, string text)
+        {
+            this.cellGrid[row, col].Text = text;
+        }
+
+        /// <summary>
+        /// Gets the rowCount.
+        /// </summary>
+        /// <returns>Integer.</returns>
+        public int GetRowCount()
+        {
+            return this.rowCount;
+        }
+
+        /// <summary>
+        /// Gets the columnCount.
+        /// </summary>
+        /// <returns>Integer.</returns>
+        public int GetColumnCount()
+        {
+            return this.columnCount;
+        }
+
+        /// <summary>
+        /// Demo for the button.
+        /// </summary>
+        public void Demo()
+        {
+            Random rnd = new Random();
+            int row = 0;
+            int col = 0;
+
+            for (int i = 0; i < 50; i++)
+            {
+                col = rnd.Next(0, 26);
+                row = rnd.Next(0, 50);
+
+                this.cellGrid[row, col].Text = "Yay! It worked!";
+            }
+
+            row = 1;
+            for (int i = 0; i < 50; i++)
+            {
+                this.cellGrid[i, 1].Text = "This is cell B " + row; // Setting cells in column B to "This is cell B #
+                row++;
+            }
+
+            row = 1;
+            for (int i = 0; i < 50; i++)
+            {
+                this.cellGrid[i, 0].Text = "=B" + row; // Setting the column A = to adjacent B cells.
+                row++;
+            }
+        }
+
+        /// <summary>
+        /// This method is for testing the get and setting function for value.
+        /// </summary>
+        public void TestForSetValue()
+        {
+            this.cellGrid[0, 0].Value = "test";
+            this.cellGrid[1, 1].Value = "=Test";
+            this.cellGrid[3, 3].Value = "=A1";
+            this.cellGrid[10, 10].Value = "=B33";
+        }
+
+        /// <summary>
+        /// This will identify the text of a cell and depending if the text string starts with "=" will update the value of the cell.
+        /// If the string starts with "=" this will set the value of the cell to equal another cell's value. Otherwise, the value
+        /// will be set equal to the text.
+        /// </summary>
+        /// <param name="sender.">Spreadsheet.</param>
+        /// <param name="e">Cell argument.</param>
+        private void SpreadsheetCellValue_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Text")
+            {
+                SpreadsheetCellValue cell = (SpreadsheetCellValue)sender;
+                string evalutedText = cell.Text;
+                char columnCharacter = Convert.ToChar(cell.ColumnIndex + 65);
+                string cellName = columnCharacter.ToString().ToUpper() + (cell.RowIndex + 1).ToString(); // Converts cell location to a cellName
+
+                if (CheckText(evalutedText))
+                {
+                    // string sub = evalutedText.Substring(2, evalutedText.Length - 2);
+                    // char colSymbol = evalutedText[1];
+                    // int row = int.Parse(sub) - 1;
+                    // int col = (int)colSymbol - 'A';
+                    // cell.Value = this.cellGrid[row, col].Value; // If the string has 3 characters, this assumes it is in the format "=A#"
+                    string newValue = this.EvaluateText(cell.Text);
+                    this.SetCellText(cell.RowIndex, cell.ColumnIndex, evalutedText);
+                    cell.Value = newValue; // Evaluates the cell text if it starts with "=" and returns the double as a string
+                    this.expressionTree.SetVariable(cellName, Convert.ToDouble(newValue)); // This is assuming that the user inputs the formula without errors and adds the cell value to the dictionary
+            }
+                else
+                {
+                    this.SetCellText(cell.RowIndex, cell.ColumnIndex, cell.Text);
+                    cell.Value = cell.Text; // If the string does not start with "=" then the string value will be set to the text of the cell.
+
+                    double number;
+                    if (double.TryParse(cell.Value, out number))
+                    {
+                        this.expressionTree.SetVariable(cellName, number); // If cell value can be parsed to double, set cellName, double
+                    }
+                    else
+                    {
+                      this.expressionTree.SetVariable(cellName, 0); // If cell value cannot be parsed to double, set cellName, 0 (default)
+                    }
+                }
+            }
+
+            this.PropertyChangedValue(sender, new PropertyChangedEventArgs("Value"));
+        }
+    }
+}
---
+++ HW8_Spreadsheet Formula Evalution/Spreadsheet_Koji_Natsuhara/Form1.cs
@@ -0,0 +1,136 @@
+﻿// <copyright file="Form1.cs" company="Koji Natsuhara (ID: 11666900)">
+// Copyright (c) Koji Natsuhara (ID: 11666900). All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CptS321;
+
+namespace Spreadsheet_Koji_Natsuhara
+{
+    /// <summary>
+    /// This class will intitalize the winform application.
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Spreadsheet object as a member variable.
+        /// </summary>
+        private Spreadsheet mainSpreadSheet;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Form1"/> class.
+        /// </summary>
+        public Form1()
+        {
+            this.InitializeComponent();
+            this.mainSpreadSheet = new Spreadsheet(50, 26);
+
+            this.mainSpreadSheet.PropertyChangedValue += this.MainSpreadSheet_PropertyChangedValue;
+
+            this.dataGridView1.CellBeginEdit += this.DataGridView1_CellBeginEdit;
+            this.dataGridView1.CellEndEdit += this.DataGridView1_CellEndEdit;
+
+            this.SetupGridColumns();
+            this.SetupGridRows();
+        }
+
+        /// <summary>
+        /// Cells in UI grid values are set to the values of the logic engine cell.
+        /// </summary>
+        /// <param name="sender">sender.</param>
+        /// <param name="e">e.</param>
+        private void MainSpreadSheet_PropertyChangedValue(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Value")
+            {
+                SpreadsheetCell cell = (SpreadsheetCell)sender;
+                this.dataGridView1[cell.ColumnIndex, cell.RowIndex].Value = cell.Value;
+            }
+        }
+
+        /// <summary>
+        /// Handles an event to reflect that the current DataGridViewCell is being editied. When the user starts to edit the cell,
+        /// the cell value should change to the Text property of the cell.
+        /// </summary>
+        /// <param name="sender">Data Grid View.</param>
+        /// <param name="e">Event Argument.</param>
+        private void DataGridView1_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        {
+            int row = e.RowIndex;
+            int col = e.ColumnIndex;
+            DataGridViewCell cell = this.dataGridView1[col, row];
+            cell.Value = this.mainSpreadSheet.GetCell(row, col).Text;
+        }
+
+        /// <summary>
+        /// CellEndEdit event occurs only when the user finishes editing the cell, it must go back to the Value.
+        /// </summary>
+        /// <param name="sender">The Data Grid View.</param>
+        /// <param name="e">The event args holding the cell information.</param>
+        private void DataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            int row = e.RowIndex;
+            int col = e.ColumnIndex;
+            DataGridViewCell cell = this.dataGridView1[col, row];
+
+            this.mainSpreadSheet.SetCellText(row, col, cell.Value.ToString());
+            cell.Value = this.mainSpreadSheet.GetCell(row, col).Value;
+        }
+
+        /// <summary>
+        /// This function will setup the columns of the dataGridView1 object from A to Z.
+        /// </summary>
+        private void SetupGridColumns()
+        {
+            int alpha = 65; // ASCII VALUE 65 == A
+
+            for (int i = 0; i < 26; i++)
+            {
+                char c = Convert.ToChar(alpha); // CONVERT INT TO CHAR
+                this.dataGridView1.Columns.Add(c.ToString(), c.ToString()); // Setting header text and name to c.
+                alpha++; // INCREMENT alpha
+            }
+        }
+
+        /// <summary>
+        /// This function will setup the rows of the dataGridView1 object from 1 to 50.
+        /// </summary>
+        private void SetupGridRows()
+        {
+            int j = 1;
+            for (int i = 0; i < 50; i++)
+            {
+                this.dataGridView1.Rows.Add();
+                this.dataGridView1.Rows[i].HeaderCell.Value = j.ToString(); // Names the header cell values.
+                j++;
+            }
+        }
+
+        /// <summary>
+        /// Form1_Load Function.
+        /// </summary>
+        /// <param name="sender">sender.</param>
+        /// <param name="e">e.</param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// This button if clicked will perform a demo test.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">E.</param>
+        private void DemoButton_Click(object sender, EventArgs e)
+        {
+            this.mainSpreadSheet.Demo();
+        }
+    }
+}
