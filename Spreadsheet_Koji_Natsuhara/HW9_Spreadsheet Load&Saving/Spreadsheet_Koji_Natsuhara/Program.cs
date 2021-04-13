33fff9d5b908d261abb5f7c3a612ca814350b76e
KNatsuhara <kojiman@live.com>
2021-04-13 16:09:29 -0700

    Implemented Cell Dependencies

---
+++ Spreadsheet_Koji_Natsuhara/HW9_Spreadsheet Load&Saving/Spreadsheet_Koji_Natsuhara/Program.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="Program.cs" company="Koji Natsuhara (ID: 11666900)">
+// Copyright (c) Koji Natsuhara (ID: 11666900). All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CptS321
+{
+    /// <summary>
+    /// This class will run the main method that will start the application.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
